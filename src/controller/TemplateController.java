fd6a84e97eec311ee4b099cf53ec9c66ec15f49f
Janio <janiojunior@gmail.com>
2018-11-08 19:46:57 -0300

    template .. novo menu

--- src/controller/TemplateController.java
+++ src/controller/TemplateController.java
@@ -7,21 +7,45 @@ import java.util.ResourceBundle;
 import application.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class TemplateController implements Initializable {
-
+	public static Parent clienteView;
+	public static Parent cidadeView;
+	
+    @FXML
+    private ScrollPane scrollPane;
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		scrollPane.setFitToHeight(true);
+		scrollPane.setFitToWidth(true);
+		try {
+			cidadeView = FXMLLoader.load(Main.class.getResource("/view/CadastroCidade.fxml"));
+			clienteView = FXMLLoader.load(Main.class.getResource("/view/CadastroCliente.fxml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@FXML
 	void handleCidade(ActionEvent event) throws IOException {
-		Main.trocaTela(2);
+		VBox vbox = new VBox();
+		vbox.setAlignment(Pos.TOP_CENTER);
+		vbox.getChildren().add(cidadeView);
+		scrollPane.setContent(vbox);
 	}
 
 	@FXML
 	void handleCliente(ActionEvent event) throws IOException {
-		Main.trocaTela(1);
+		VBox vbox = new VBox();
+		vbox.setAlignment(Pos.TOP_CENTER);
+		vbox.getChildren().add(clienteView);
+		scrollPane.setContent(vbox);
 	}
 }
